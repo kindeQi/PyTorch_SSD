ccd95d966d0ef32a20d84b406d998195b987ecd2
Rui Zhang <zhan495@usc.edu>
2019-02-03 01:39:54 -0800

    verified iou mask and loss

--- SSDloss.py
+++ SSDloss.py
@@ -58,6 +58,9 @@ def get_prior_box():
                 prior_box.append((left, top, hi, wi))
     prior_box = torch.tensor(prior_box)
 
+    # make sure the prior_box in the range of [0, 1]
+    prior_box = prior_box.clamp(0, 1)
+
     return prior_box
 
 
@@ -74,24 +77,17 @@ def get_iou(bbox, prior_box):
 
     imgs_bbox: List[List[cx, cy, w, h]]
     prior_box: tensor, (8732, 4)
+
+    imgs_bbox: List[List[xyxy]]
+    prior_box: tensor, (8732, 4)
     '''
-#     gt = torch.FloatTensor(imgs_bbox[id])
     gt = torch.FloatTensor(bbox.copy())
-    # gt = bbox.clone()
-    
-    # rescale
-    gt /= 300
-    
-    # transform gt
-    gt[:, 2:] = gt[:, :2] + gt[:, 2:]
-    
-    # transform prior_box
-#     prior_box_ = torch.cat([prior_box[:, :2] - prior_box[:, 2:] / 2, prior_box[:, :2] + prior_box[:, 2:] / 2], dim=1)
-    prior_box_ = torch.cat([(prior_box[:, 1] - prior_box[:, 2] / 2).unsqueeze(1), 
-                            (prior_box[:, 0] - prior_box[:, 3] / 2).unsqueeze(1), 
-                            (prior_box[:, 1] + prior_box[:, 2] / 2).unsqueeze(1), 
-                            (prior_box[:, 0] + prior_box[:, 3] / 2).unsqueeze(1)], dim=1)
-    
+    prior_box_ = torch.cat(
+        [prior_box[:, 0:2] - prior_box[:, 2:4] / 2,
+        prior_box[:, 0:2] + prior_box[:, 2:4] / 2],
+        dim=1
+    )
+
     A = gt.size(0)
     B = prior_box_.size(0)
     
@@ -107,6 +103,42 @@ def get_iou(bbox, prior_box):
     area_b = ((prior_box_[:, 2]-prior_box_[:, 0]) *
               (prior_box_[:, 3]-prior_box_[:, 1])).unsqueeze(0).expand_as(inter)  # [A,B]
     union = area_a + area_b - inter
+
+    return inter / union
+
+
+# #     gt = torch.FloatTensor(imgs_bbox[id])
+#     gt = torch.FloatTensor(bbox.copy())
+#     # gt = bbox.clone()
+    
+#     # rescale
+#     gt /= 300
+    
+#     # transform gt
+#     gt[:, 2:] = gt[:, :2] + gt[:, 2:]
+    
+#     # transform prior_box
+# #     prior_box_ = torch.cat([prior_box[:, :2] - prior_box[:, 2:] / 2, prior_box[:, :2] + prior_box[:, 2:] / 2], dim=1)
+#     prior_box_ = torch.cat([(prior_box[:, 1] - prior_box[:, 2] / 2).unsqueeze(1), 
+#                             (prior_box[:, 0] - prior_box[:, 3] / 2).unsqueeze(1), 
+#                             (prior_box[:, 1] + prior_box[:, 2] / 2).unsqueeze(1), 
+#                             (prior_box[:, 0] + prior_box[:, 3] / 2).unsqueeze(1)], dim=1)
+    
+#     A = gt.size(0)
+#     B = prior_box_.size(0)
+    
+#     max_xy = torch.min(gt[:, 2:].unsqueeze(1).expand(A, B, 2),
+#                        prior_box_[:, 2:].unsqueeze(0).expand(A, B, 2))
+#     min_xy = torch.max(gt[:, :2].unsqueeze(1).expand(A, B, 2),
+#                        prior_box_[:, :2].unsqueeze(0).expand(A, B, 2))
+#     inter = torch.clamp((max_xy - min_xy), min=0)
+#     inter = inter[:, :, 0] * inter[:, :, 1]
+    
+#     area_a = ((gt[:, 2]-gt[:, 0]) *
+#               (gt[:, 3]-gt[:, 1])).unsqueeze(1).expand_as(inter)  # [A,B]
+#     area_b = ((prior_box_[:, 2]-prior_box_[:, 0]) *
+#               (prior_box_[:, 3]-prior_box_[:, 1])).unsqueeze(0).expand_as(inter)  # [A,B]
+#     union = area_a + area_b - inter
     
     return inter / union
 
@@ -133,12 +165,14 @@ def get_target(iou, prior_box, img, bbox, label):
 
     # no need to add 1, cause already [0, 20] to 21 classes
     # cls_target[pos_mask] += 1
-    bbox_target = encode_bbox(prior_box, bbox_target)
+    bbox_target = encode(bbox_target, prior_box, [0.1, 0.2])
 
     return pos_mask, cls_target, bbox_target
 
-
-def encode_bbox(prior_box, bbox_target):
+def encode_bbox_deprecated(prior_box, bbox_target):
+    '''
+    deprecated method, us encode(pred_bbox, priors, variance) instead
+    '''
     # 5. encode the bbox_target
 
     variance = [0.1, 0.2]
@@ -157,6 +191,31 @@ def encode_bbox(prior_box, bbox_target):
 
     return bbox_target
 
+# test encode and decode module
+def encode(bbox, priors, variance):
+    '''
+    Arguments:
+    bbox: (8732, 4), size / 300, xyxy format
+    priors:    (8732, 4), cxcy + wh format
+    '''
+    cxcy = (bbox[:, 2:4] + bbox[:, 0:2]) / 2
+    cxcy = (cxcy - priors[:, 0:2]) / (priors[:, 2:4] * variance[0])
+    
+    wh = (bbox[:, 2:4] - bbox[:, 0:2])
+    wh = torch.log(wh / priors[:, 2:4]) / variance[1]
+    
+    return torch.cat([cxcy, wh], dim=1)
+
+def decode(bbox, priors, variance):
+    cxcy =  bbox[:, 0:2] * variance[0] * priors[:, 2:4] + priors[:, 0:2]
+    wh = torch.exp(bbox[:, 2:4] * variance[1]) * priors[:, 2:4]
+    
+    x1y1 = cxcy - wh / 2
+    x2y2 = cxcy + wh / 2
+    
+    return torch.cat([x1y1, x2y2], 1)
+
+
 def loss(cls_pred, loc_pred, pos_mask, cls_target, bbox_target):
     '''
     one image a time
@@ -169,35 +228,36 @@ def loss(cls_pred, loc_pred, pos_mask, cls_target, bbox_target):
     ratio = 3
     num_pos = torch.sum(pos_mask)
     num_neg = ratio * num_pos
+    neg_mask = 1 - pos_mask
 
     if num_pos == 0:
         return torch.tensor(0)
 
     # loss of class including hard negative mining
-    loc_criterion, cls_criterion = nn.SmoothL1Loss(), nn.CrossEntropyLoss(reduce=False)
+    loc_criterion, cls_criterion = nn.SmoothL1Loss(reduce=False), nn.CrossEntropyLoss(reduce=False)
+
+    loss_cls = cls_criterion(cls_pred, cls_target.long())
     
-    loss_cls_pos = cls_criterion(cls_pred[pos_mask], cls_target.long()[pos_mask])
+    loss_cls_pos = loss_cls[pos_mask]
     loss_cls_pos = torch.sum(loss_cls_pos)
 
-    conf_loss_neg = cls_criterion(cls_pred[1 - pos_mask], cls_target.long()[1 - pos_mask])
-    val, arg = torch.sort(conf_loss_neg, descending=True)
-    loss_cls_neg = torch.sum(conf_loss_neg[arg[:num_neg]])
+    conf_loss_neg = loss_cls[neg_mask]
+    _, neg_idx = torch.sort(conf_loss_neg, descending=True)
+    loss_cls_neg = torch.sum(conf_loss_neg[neg_idx[:num_neg]])
 
 #     loss_cls = loss_cls_neg + loss_cls_pos 
 
 #     without hard negative mining
-    loss_cls = loss_cls_pos
+    loss_cls = loss_cls_pos + loss_cls_neg
 
     # loss of location
     loss_loc = loc_criterion(loc_pred[pos_mask], bbox_target[pos_mask])
+    loss_loc = torch.sum(loss_loc)
+    
     # loss = loss_loc + loss_cls / float(num_pos)
-    
-    # print(loss_cls.data / float(num_pos), loss_loc)
-    
-    # print(cls_pred[pos_mask][0], cls_target[pos_mask][0])
-    # print(cls_criterion(cls_pred[pos_mask][0], cls_target[pos_mask][0]))
+    print(loss_cls_pos, loss_cls_neg, loss_loc)
 
-    return loss_loc, loss_cls / float(num_pos)
+    return loss_loc / float(num_pos), loss_cls / float(num_pos)
 
 
 if __name__ == "__main__":
@@ -219,5 +279,15 @@ if __name__ == "__main__":
 
     # loss(cls_pred, loc_pred, pos_mask, cls_target, bbox_target)
 
+    bbox = [[ 0.383333325386047,  0.167582422494888,  0.579166650772095,
+           0.543956041336060],
+         [ 0.185416668653488,  0.211538463830948,  0.837499976158142,
+           0.920329689979553]]
+    bbox = np.array(bbox)
+    label= [15, 13]
+
     prior_box = get_prior_box()
-    print(prior_box[38* 38 * 4: 38 * 38 * 4 + 10, :])
+    iou = get_iou(bbox, prior_box)
+    pos_mask = iou > 0.5
+    iou[pos_mask].shape
+    print(iou[pos_mask])
